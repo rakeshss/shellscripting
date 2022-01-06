15958c0da03ed2ac3baedaeebcf2ceaf3d05f10b
Rakesh <rakesh@Rakeshs-MacBook-Pro-2.local>
2022-01-06 09:20:36 +0530

    exist code in bash

---
+++ conditional/exitcode.sh
@@ -0,0 +1,20 @@
+#Standard exit codes :-
+
+# To use ,multi line comment use : ' '
+
+: '
+0 - ‘command successfully run’ (OK)
+1 - Used when there is an error but no specific number reserved to indicate what it was (general error)
+2 - Misuse of shell builtin command
+126 - Permission problem or command is not executable
+127 - No file found matching this command
+128 - Invalid exit argument given (eg exit 1.76)
+'
+
+#!/bin/sh
+
+echo "Learning basic stuff on exit code.."
+
+echo 'grepme' > afile.txt
+grep not_there afile.txt
+echo $?
